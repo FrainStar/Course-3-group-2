6a72603ecfe6df50203e66f7d113fe56d790a9eb
Frain <frainworks1@mail.ru>
2024-10-25 18:30:02 +0300

    feature(lesson11): practice complete

---
+++ Course/3rdyear/Lesson10/ConsoleApp/WebApplication1/Controllers/TaskController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication2.Controllers;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public TaskItem(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
+[ApiController]
+public class TaskController : Controller
+{
+    [HttpGet("api/tasks")]
+    public IActionResult WriteTask()
+    {
+        return Ok(new TaskItem(1, "Ilya"));
+    }
+}
