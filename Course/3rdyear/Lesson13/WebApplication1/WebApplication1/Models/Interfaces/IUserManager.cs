4d88ea56e907b76f5e738ac6f1f4a54e93291ac3
Frain <frainworks1@mail.ru>
2024-11-12 15:58:38 +0300

    feature(lesson13): practice complete, but without dependencies in database

---
+++ Course/3rdyear/Lesson13/WebApplication1/WebApplication1/Models/Interfaces/IUserManager.cs
@@ -0,0 +1,6 @@
+namespace WebApplication1.Models.Interfaces;
+
+public class IUserManager
+{
+    
+}
---
+++ Course/3rdyear/Lesson13/WebApplication1/WebApplication1/Models/Models/DatabaseContext.cs
@@ -0,0 +1,6 @@
+namespace WebApplication1.Models.Models;
+
+public class DatabaseContext
+{
+    
+}
